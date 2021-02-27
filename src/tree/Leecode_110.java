075d7718539ffc04e43938fdf3f93d6adc7ea7bf
Bertking <kingcheeng163@gmail.com>
2021-02-27 18:18:47 +0800

    Update Leecode_110.java

--- src/tree/Leecode_110.java
+++ src/tree/Leecode_110.java
@@ -5,12 +5,43 @@ package tree;
  * @Package tree
  * @Description: ReviewLeeCode
  * @date 2021/2/27-10:56 上午
- * @problem
+ * @problem 110. 平衡二叉树
+ *
+ * 平衡二叉(搜索)树是一种结构平衡的二叉搜索树，它是一种每个节点的左右两子树『高度差』都不超过1的二叉树。
+ *
+ * 由于涉及到树高度的问题，所以推荐先做【104-二叉树的最大深度】
+ *
  */
 public class Leecode_110 {
 
     static class Solution1{
+        /**
+         * 按照『平衡二叉树』的定义来解
+         * @param root
+         * @return
+         */
+        public boolean isBalanced(TreeNode root){
+            if(root == null) return true;
+//            if(Math.abs(height(root.left) - height(root.right)) > 1){
+//                return false;
+//            }
+            return Math.abs(height(root.left) - height(root.right)) > 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+        /**
+         * 求树的高(深)度
+         * @param root
+         * @return
+         */
+        private int height(TreeNode root){
+            if(root == null){
+                return 0;
+            }
 
+            int left = height(root.left);
+            int right = height(root.right);
+
+            return Math.max(left,right)+1;
+        }
     }
 
 
