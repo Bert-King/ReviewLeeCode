778f1c0966c705b56a1e9352647ddddb395fed72
Bertking <kingcheeng163@gmail.com>
2021-03-10 20:31:25 +0800

    Create ListNode.java

---
+++ src/tree/ListNode.java
@@ -0,0 +1,16 @@
+package tree;
+
+/**
+ * @author bertking
+ * @Package tree
+ * @Description: ReviewLeeCode
+ * @date 2021/3/10-7:35 下午
+ * @problem 链表的结点
+ */
+public class ListNode {
+    int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
