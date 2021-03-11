87c6f9d93304079d5680622103ad92479d44c589
Bertking <kingcheeng163@gmail.com>
2021-03-11 10:18:51 +0800

    Create Leetcode_141.java

---
+++ src/linkedlist/Leetcode_141.java
@@ -0,0 +1,41 @@
+package linkedlist;
+
+import tree.ListNode;
+
+/**
+ * @author bertking
+ * @Package linkedlist
+ * @Description: ReviewLeeCode
+ * @date 2021/3/10-10:56 下午
+ * @problem 141. 环形链表
+ * @level Easy
+ */
+public class Leetcode_141 {
+    /**
+     * 快慢指针：若链表有环，则快指针能追上慢指针。
+     */
+    static class Solution{
+        public boolean hasCycle(ListNode head){
+            if(head == null ){
+                return false;
+            }
+
+            if(head.next == null){
+                return false;
+            }
+
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while(fast != null && fast.next != null){
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if(slow == fast){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
