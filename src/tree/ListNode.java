6fa43258066d4fd592bc4f375efad6e6ee801cee
Bertking <kingcheeng163@gmail.com>
2021-03-11 15:06:00 +0800

    Update ListNode.java

--- src/tree/ListNode.java
+++ src/tree/ListNode.java
@@ -8,9 +8,18 @@ package tree;
  * @problem 链表的结点
  */
 public class ListNode {
-    int val;
+    public int val;
     public ListNode next;
-     ListNode() {}
-     ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
