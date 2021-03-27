20f62b7493c55d41c8098a454d849d8c19d40b40
Bertking <kingcheeng163@gmail.com>
2021-03-27 10:12:46 +0800

    Create Leetcode_6.java

---
+++ src/strings/Leetcode_6.java
@@ -0,0 +1,93 @@
+package strings;
+
+/**
+ * @author bertking
+ * @Package strings
+ * @Description: ReviewLeeCode
+ * @date 2021/3/26-3:56 下午
+ * @problem 6. Z 字形变换
+ * @level Medium
+ */
+public class Leetcode_6 {
+
+    /**
+     * 将其分为 『竖列』和 『斜列』来看
+     */
+    class Solution {
+        public String convert(String s, int numRows) {
+            int len = s.length();
+            if (len == 0 || numRows < 1) {
+                return "";
+            }
+
+            if (numRows == 1) {
+                return s;
+            }
+
+            StringBuilder[] res = new StringBuilder[numRows];
+            for (int i = 0; i < numRows; ++i) {
+                res[i] = new StringBuilder();
+            }
+
+            /**
+             * 对于输入的字符串S，其下标 i / (numRows-1)
+             * 如果结果为 偶数 or 0,则代表该下标代表的字符在Z字型中属于 『竖列』。
+             * 如果结果为 奇数，则代表该下标代表的字符在Z字型中属于 『斜列』(PS: Z字型的中间倾斜部分)。
+             *
+             * 另外，如果当前字符属于『竖列』，则按照正序依次保存在数组中；
+             * 如果属于『斜列』，则逆序保存在数组中。
+             */
+            for (int i = 0; i < len; i++) {
+
+                int ans = i / (numRows - 1);
+                int remain = i % (numRows - 1);
+                if (ans % 2 == 0) {
+                    res[remain].append(s.charAt(i)); // //按余数正序保存
+                } else {
+                    res[numRows - remain - 1].append(s.charAt(i)); // //按余数倒序保存
+                }
+
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < numRows; i++) {
+                stringBuilder.append(res[i].toString());
+            }
+
+            return stringBuilder.toString();
+        }
+    }
+
+    class Solution1 {
+        public String convert(String s, int numRows) {
+            if (numRows == 1) {
+                return s;
+            }
+
+            int length = s.length();
+
+            StringBuilder sb = new StringBuilder();
+            /**
+             * 通过总结,我们可以发现：周期为 cycle = 2 * (numRows-1)
+             */
+            int cycleLen = 2 * (numRows - 1);
+
+            for (int i = 0; i < numRows; i++) {
+                for (int j = i; j < length; j += cycleLen) {
+                    sb.append(s.charAt(j));
+
+                    if(i > 0 && i < numRows-1){
+                        int sec = j + 2*(numRows-i-1);
+                        if(sec < length){
+                            sb.append(s.charAt(sec));
+                        }
+                    }
+                }
+            }
+
+            return sb.toString();
+        }
+
+    }
+
+}
