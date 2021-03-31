de58d7efc1e8a3dadd163a752b68af39ce50ef69
Bertking <kingcheeng163@gmail.com>
2021-03-31 22:03:43 +0800

    Update leetcode_88.java

--- src/array/merge/leetcode_88.java
+++ src/array/merge/leetcode_88.java
@@ -10,9 +10,23 @@ package array.merge;
  */
 public class leetcode_88 {
 
+    /**
+     * 从尾部开始对比
+     */
     class Solution {
         public void merge(int[] nums1, int m, int[] nums2, int n) {
-            int pos = (m--)+(n--)-1;
+//            int pos = (m--)+(n--)-1;
+
+            int pos = m + n -1;
+
+            /**
+             * 这里需要注意一下，题目中说明:初始化 nums1 和 nums2 的元素数量分别为 m 和 n
+             * 这里进行 减1 操作是为了得到两者的尾部下标。
+             */
+            m = m -1;
+            n = n-1;
+
+
             while(m >= 0 && n >= 0){
                 if(nums1[m] > nums2[n]){
                     nums1[pos--] = nums1[m--];
