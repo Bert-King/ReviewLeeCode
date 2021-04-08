c19a3872d008744339dc7f28c7420539039ea90b
Bertking <kingcheeng163@gmail.com>
2021-04-08 19:54:53 +0800

    Update Leetcode_121.java

--- src/stock/Leetcode_121.java
+++ src/stock/Leetcode_121.java
@@ -7,9 +7,15 @@ package stock;
  * @date 2021/4/7-4:50 下午
  * @problem 121. 买卖股票的最佳时机
  * @level Easy
+ *
+ * 注意这道题：只能交易一次
  */
 public class Leetcode_121 {
 
+    /**
+     * 动态规划:
+     * 前i天的最大收益 = max{前i-1天的最大收益，第i天的价格-前i-1天中的最小价格}
+     */
     static class Solution{
         public int maxProfit(int prices[]){
             int maxProfit = 0;
